ab15ec387fa0ad7944ec6b698c28f9afbef3e9eb
keenguy <dujcel@icloud.com>
2020-01-15 16:39:33 +0800

    Site updated: 2020-01-15 16:39:33

--- blog.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd ~/Coding/github/gynote.github.io/
-rm -rf source/_posts/*
-cp -pr ~/Works/SyncNotes/* source/_posts/
-hexo clean && hexo g
-cp -r ~/Works/SyncNotes/.pastes public/images
